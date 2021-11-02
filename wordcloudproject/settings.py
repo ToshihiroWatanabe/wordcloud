74566a11235a29f223090d43c0dd079443dad2cc
ToshihiroWatanabe <79039863+ToshihiroWatanabe@users.noreply.github.com>
2021-11-02 18:28:10 +0900

    update: Herokuデプロイ用の記述を追加

--- wordcloudproject/settings.py
+++ wordcloudproject/settings.py
@@ -5,7 +5,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-2_g3hzbqt8lse6xhj$rhdwn@8o%aiwzk6y&756*6#0$0x0=3_n'
 
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [os.environ['ALLOWED_HOSTS']]
 
@@ -27,6 +27,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'wordcloudproject.urls'
@@ -74,6 +75,24 @@ USE_L10N = True
 
 USE_TZ = True
 
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# django-herokuの設定
+try:
+    from .local_settings import *
+except ImportError:
+    pass
+
+if not DEBUG:
+    import django_heroku
+    django_heroku.settings(locals())
